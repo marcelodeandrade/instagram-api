a0c29bc02d994301cb77d605b071930754657e05
Marcelo de Andrade <andrade.asmarcelo@gmail.com>
2017-12-21 15:23:08 -0300

    Requests methods refactored

--- src/Client.php
+++ src/Client.php
@@ -14,11 +14,8 @@ class Client
     private $scope;
 
     private $endpoints = [
-        'auth' => 'https://api.instagram.com/oauth/authorize/
-                    ?client_id=%s
-                    &redirect_uri=%s
-                    &response_type=code
-                    &scope=%s',
+        'auth' => 'https://api.instagram.com/oauth/authorize/?client_id=%s&redirect_uri=%s&response_type=code&scope=%s',
+        'access_token' => 'https://api.instagram.com/oauth/access_token',
         'media' => 'https://api.instagram.com/v1/media/%s/likes',
     ];
 
@@ -29,15 +26,31 @@ class Client
         }, $options, array_keys($options));
     }
 
-    public function makeRequest(array $options)
+    public function makeRequest(string $request, string $url, array $options)
     {
-        $response = (new \GuzzleHttp\Client)->request($options['request_type'], $options['url']);
-        return $response->getBody();
+        return (string) (new \GuzzleHttp\Client)
+        ->request($request, $url, $options)
+        ->getBody();
     }
     
     public function getAuthURI(): string
     {
         return sprintf($this->endpoints['auth'], $this->client_id, $this->redirect_uri, $this->scope);
+    }
+
+    public function requestAccessToken(string $code): void
+    {
+        $options = [
+            'client_id'     => $this->client_id,
+            'client_secret' => $this->client_secret,
+            'grant_type'    => 'authorization_code',
+            'redirect_uri'  => $this->redirect_uri,
+            'code'          => $code,
+        ];
+
+        $requestAccessToken = $this->makeRequest('POST', $this->endpoints['access_token'], ['form_params' => $options]);
+
+        $this->setAccessToken((json_decode($requestAccessToken))->access_token);
     }
 
     public function setAccessToken(string $acess_token): void
